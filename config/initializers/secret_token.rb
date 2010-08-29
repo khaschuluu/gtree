33ec06a285ff0e6a5468cd54602037e2ce0fa6d9
Ochirkhuyag <ochkoo@gmail.com>
2010-08-29 16:49:05 +0900

    rails 3 rc migration changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gtree::Application.config.secret_token = '96f381d7ca044b72c7cc27ae883e1146f8395cd8c255005e29c2db95d5afca349569012790a332d8aa891bc622a7e43fbd8e18a0e42480f810ea66d526810117'
