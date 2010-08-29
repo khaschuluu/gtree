5423b4e55170bc8d667defb8095f89e11375b2cd
Ochirkhuyag <ochkoo@gmail.com>
2010-08-29 18:13:29 +0800

    preview added for tab

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
+}
--- public/javascripts/tablature.js
+++ public/javascripts/tablature.js
@@ -3,10 +3,7 @@ var ddd  = "-";
 var dddd  = "--";
 function write_tab(s, f)
 {
-	
-	FR = f; strng = s;
-
-	if (FR>9) 
+	if (f>9) 
 	{
 		if(dddd=="--"){dddd="---"};
 	}
@@ -14,16 +11,36 @@ function write_tab(s, f)
 	{
 		if(dddd=="---"){dddd="--"};
 	}
-	if(strng==6){str6 += FR + ddd;} else {str6 += dddd;}
-	if(strng==5){str5 += FR + ddd;} else {str5 += dddd;}
-	if(strng==4){str4 += FR + ddd;} else {str4 += dddd;}
-	if(strng==3){str3 += FR + ddd;} else {str3 += dddd;}
-	if(strng==2){str2 += FR + ddd;} else {str2 += dddd;}
-	if(strng==1){str1 += FR + ddd;} else {str1 += dddd;}
+	if(s==6){str6 += f + ddd;} else {str6 += dddd;}
+	if(s==5){str5 += f + ddd;} else {str5 += dddd;}
+	if(s==4){str4 += f + ddd;} else {str4 += dddd;}
+	if(s==3){str3 += f + ddd;} else {str3 += dddd;}
+	if(s==2){str2 += f + ddd;} else {str2 += dddd;}
+	if(s==1){str1 += f + ddd;} else {str1 += dddd;}
 	display();
 	
 }
 function display()
 {
-	document.getElementById('tab_tab').value = (str6 + "\n" + str5 + "\n" + str4 + "\n" + str3 + "\n" + str2 + "\n" + str1 + "\n");
+	document.getElementById('tab_tab').value = (str1 + "\n" + str2 + "\n" + str3 + "\n" + str4 + "\n" + str5 + "\n" + str6 + "\n");
+}
+function clear_all()
+{
+    str6 = "";
+    str5 = "";
+    str4 = "";
+    str3 = "";
+    str2 = "";
+    str1 = "";
+    display();
+}
+function back_clear()
+{
+    str6 = str6.slice(0,-1);
+    str5 = str5.slice(0,-1);
+    str4 = str4.slice(0,-1);
+    str3 = str3.slice(0,-1);
+    str2 = str2.slice(0,-1);
+    str1 = str1.slice(0,-1);
+    display();
 }
