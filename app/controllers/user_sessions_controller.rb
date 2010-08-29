85e467131ba4da72f0c00aa603603b1b930bc2d3
Ochirkhuyag <ochkoo@gmail.com>
2010-08-29 15:49:34 +0800

    login action fixes

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,79 +1,26 @@
 class UserSessionsController < ApplicationController
   layout "tabs"
 
-  # GET /user_sessions
-  # GET /user_sessions.xml
-  def index
-    @user_sessions = UserSession.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @user_sessions }
-    end
-  end
-
-  # GET /user_sessions/1
-  # GET /user_sessions/1.xml
-  def show
-    @user_session = UserSession.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user_session }
-    end
-  end
-
-  # GET /user_sessions/new
-  # GET /user_sessions/new.xml
+  before_filter :require_user, :only => :destroy
+  
   def new
     @user_session = UserSession.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user_session }
-    end
   end
-
-  # GET /user_sessions/1/edit
-  def edit
-    @user_session = UserSession.find(params[:id])
-  end
-
-  # POST /user_sessions
-  # POST /user_sessions.xml
+  
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = 'Successfully logged in.'
-      redirect_to root_url
+      flash[:notice] = "Signed in successfully"
+      redirect_back_or_default root_url
     else
-      render :action => 'new'
+      render :action => :new
     end
   end
-
-  # PUT /user_sessions/1
-  # PUT /user_sessions/1.xml
-  def update
-    @user_session = UserSession.find(params[:id])
-
-    respond_to do |format|
-      if @user_session.update_attributes(params[:user_session])
-        flash[:notice] = 'UserSession was successfully updated.'
-        format.html { redirect_to(@user_session) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @user_session.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /user_sessions/1
-  # DELETE /user_sessions/1.xml
+  
   def destroy
-    @user_session = UserSession.find
-    @user_session.destroy
-    flash[:notice] = "Successfully logged out"
-    redirect_to root_url
+    current_user_session.destroy
+    flash[:notice] = "Signed out successfully"
+    redirect_back_or_default new_user_session_url
   end
+
 end
