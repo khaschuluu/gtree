487adeb920abd327ce680a8f62e19c0ba6b6cc7b
khaschuluu <khaschuluu.m@gmail.com>
2010-09-06 09:36:22 +0800

    Develop tablature.js

--- public/javascripts/tablature.js
+++ public/javascripts/tablature.js
@@ -1,46 +1,34 @@
-var str6 = str5 = str4 = str3 = str2 = str1 = "";
+var str = new Array("", "", "", "", "", "");
 var ddd  = "-";
 var dddd  = "--";
+var i=0;
 function write_tab(s, f)
 {
-	if (f>9) 
-	{
-		if(dddd=="--"){dddd="---"};
-	}
-	else
-	{
-		if(dddd=="---"){dddd="--"};
-	}
-	if(s==6){str6 += f + ddd;} else {str6 += dddd;}
-	if(s==5){str5 += f + ddd;} else {str5 += dddd;}
-	if(s==4){str4 += f + ddd;} else {str4 += dddd;}
-	if(s==3){str3 += f + ddd;} else {str3 += dddd;}
-	if(s==2){str2 += f + ddd;} else {str2 += dddd;}
-	if(s==1){str1 += f + ddd;} else {str1 += dddd;}
+	if(f>9){if(dddd=="--"){dddd="---"};}
+	else{if(dddd=="---"){dddd="--"};}
+    for(i = 0; i < 6; i++){if(s==i+1){str[i] += f + ddd;} else {str[i] += dddd;}}
 	display();
 	
 }
 function display()
 {
-	document.getElementById('tab_tab').value = (str1 + "\n" + str2 + "\n" + str3 + "\n" + str4 + "\n" + str5 + "\n" + str6 + "\n");
+    document.getElementById('tab_tab').value = "";
+    for(i=0; i<6; i++){document.getElementById('tab_tab').value += str[i] + "\n";}
 }
 function clear_all()
 {
-    str6 = "";
-    str5 = "";
-    str4 = "";
-    str3 = "";
-    str2 = "";
-    str1 = "";
+    for(i=0; i<6; i++){str[i] = "";}
     display();
 }
 function back_clear()
 {
-    str6 = str6.slice(0,-1);
-    str5 = str5.slice(0,-1);
-    str4 = str4.slice(0,-1);
-    str3 = str3.slice(0,-1);
-    str2 = str2.slice(0,-1);
-    str1 = str1.slice(0,-1);
+    var bool=0;
+    for(i=0; i<6; i++) {str[i] = str[i].slice(0,-1);}
+    while(bool==0)
+    {
+        bool=1;
+        for(i=0; i<6; i++){if(str[i].charAt(str[i].length-1)!='-') {bool=0; break;}}
+        if(bool==0){for(i=0; i<6; i++){str[i] = str[i].slice(0,-1);}}
+    }
     display();
 }
