95e9ccf6775b776e26c17c9e7a6620781b629e9d
khaschuluu <khaschuluu.m@gmail.com>
2010-09-08 12:17:05 +0800

    Add notice animation use jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,3 +8,18 @@ function toggle_visibility(id) {
     else
         e.style.display = 'block';
 }
+
+$(function () {
+	var $alert = $('#alert');
+	if($alert.length)
+	{
+		var alerttimer = window.setTimeout(function () {
+			$alert.trigger('click');
+		}, 3000);
+		$alert.animate({height: $alert.css('line-height') || '50px'}, 200)
+		.click(function () {
+			window.clearTimeout(alerttimer);
+			$alert.animate({height: '0'}, 200);
+		});
+	}
+});
